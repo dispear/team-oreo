d3ccc7f2f8d4660afe84838bd7f6192ec3eebf3a
sungmini9755 <sungmini9755@naver.com>
2024-08-01 23:39:00 +0900

    20240801

---
+++ personal_problem/sungmin/test20240801/시저암호.java
@@ -0,0 +1,29 @@
+// 시저 암호
+
+package com.ohgiraffers.test20240801;
+
+public class 시저암호 {
+        public String solution(String s, int n) {
+            String answer = "";
+            for(int i = 0; i < s.length(); i++ ) {
+                int num = Integer.valueOf(s.charAt(i));
+                if(num >= 65 && num <= 90) {
+                    num += n;
+                    if (num > 90) {
+                        num-=26;
+                    }
+                    answer += (char)num;
+                } else if (num >= 97 && num <= 122) {
+                    num += n;
+                    if (num > 122) {
+                        num-=26;
+                    }
+                    answer += (char)num;
+                } else if(num == 32) {
+                    answer += " ";
+                }
+            }
+            return answer;
+        }
+    }
+
