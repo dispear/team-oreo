9a8ae78e96afb404a8a816159fa7b324aae03a74
hyomee2 <s5763305@naver.com>
2024-08-01 23:48:17 +0900

    권형미/240801/문자열 내 p와 y의 개수

---
+++ personal_problem/hyoungmi/240801/문자열 내 p와 y의 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int countP = 0;
+        int countY = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) =='p' || s.charAt(i) == 'P')
+                countP++;
+            if (s.charAt(i) == 'y' || s.charAt(i) == 'Y')
+                countY++;
+        }
+
+        answer = (countP == countY) ? true : false;
+
+        return answer;
+    }
+}
