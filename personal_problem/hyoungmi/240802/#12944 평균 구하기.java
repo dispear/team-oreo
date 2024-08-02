283aa8667c1f3270e3762faa67fcfffffbc5e9b0
hyomee2 <s5763305@naver.com>
2024-08-02 12:39:31 +0900

    권형미/240802/#12944 평균 구하기

---
+++ personal_problem/hyoungmi/240802/#12944 평균 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double solution(int[] arr) {
+        double sum = 0;
+
+        for (int i = 0; i < arr.length; i++)
+            sum += arr[i];
+
+        return sum / arr.length;
+    }
+}
