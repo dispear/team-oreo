d6bbe6cc4bba54ec65482a6d37c9b299eaf02bfe
Pangtaek <zombie3632@gmail.com>
2024-08-03 18:21:23 +0900

    kwangtaek/72410/신규 아이디 추천

--- personal_problem/kwangtaek/level01/Programmers72410.java
+++ personal_problem/kwangtaek/level01/Programmers72410.java
@@ -2,6 +2,11 @@ package level01;
 
 import java.util.ArrayList;
 
+/* 문제명: 신규 아이디 추천
+*  번호: 72410
+*  링크: https://school.programmers.co.kr/learn/courses/30/lessons/72410
+* */
+
 public class Programmers72410 {
     public static void main(String[] args) {
         String inputId = "...!@BaT#*..y.abcdefghijklm";
