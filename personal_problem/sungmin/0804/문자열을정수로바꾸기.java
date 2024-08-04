4df41e4d0ecfea18c8fcd06a3e85d6874a8c848b
sungmini9755 <sungmini9755@naver.com>
2024-08-04 17:52:59 +0900

    sungmin/240804/문자열을정수로바꾸기

---
+++ personal_problem/sungmin/0804/문자열을정수로바꾸기.java
@@ -0,0 +1,8 @@
+public class 문자열을정수로바꾸기 {
+    public static void main(String[] args) {
+    }
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
