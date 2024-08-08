ae2b20f2224476b0db0cd60e3a75979c285fa08c
AYeong-Jeon <dkdud98923@gmail.com>
2024-08-08 22:25:24 +0900

    ayeong/240808/87389,12912

---
+++ personal_problem/ayeong/0808/두_정수_사이의_합.java
@@ -0,0 +1,11 @@
+
+public class 두_정수_사이의_합 {
+    public long solution(int a, int b) {
+        long answer = 0;
+        for(int i = (Math.min(a, b)) ; i <= (Math.max(a, b)) ; i++) {
+            answer += i;
+        }
+
+        return answer;
+    }
+}
