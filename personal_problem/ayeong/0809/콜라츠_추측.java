ff2d8a70f7b7504bb3b412e13b5256d53d9ccd14
AYeong-Jeon <ay98923@naver.com>
2024-08-09 18:04:40 +0900

    0809

---
+++ personal_problem/ayeong/0809/콜라츠_추측.java
@@ -0,0 +1,13 @@
+
+public class 콜라츠추측 {
+    public int solution(int num) {
+        long n = (long)num;
+
+        for(int i=0 ; i<500 ; i++) {
+            if(n==1) return i;
+            n = (n%2==0) ? n/2 : n*3+1;
+        }
+
+        return -1;
+    }
+}
