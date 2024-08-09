05ff4c1d7bd4cc96b03532dd26ffd66a96367d4f
hyomee2 <s5763305@naver.com>
2024-08-09 20:55:49 +0900

    권형미/240809/#86051 없는 숫자 더하기

---
+++ personal_problem/hyoungmi/240809/#86051 없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 45;
+
+        for (int i = 0; i < numbers.length; i++)
+            sum -= numbers[i];
+
+        return sum;
+    }
+}
