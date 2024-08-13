7c31f5e1d5d6984d115699777b04fd0f98c206f9
kookong2 <hy010309@naver.com>
2024-08-13 14:13:14 +0900

    [FEAT] 08/13 부족한 금액 계산하기 문제 풀이

--- personal_problem/daeyoon/level01/240813/부족한금액계산하기.java
+++ personal_problem/daeyoon/level01/240813/부족한금액계산하기.java
@@ -1,1 +1,13 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long totalCost = 0;
 
+        for(int i = 1; i <= count; i++){
+            totalCost += price * i;
+        }
+
+        long difference = totalCost - money;
+
+        return difference > 0 ? difference : 0;
+    }
+}
