98540465fc62f0101028a09981ebec61bf5ab33e
Pangtaek <zombie3632@gmail.com>
2024-08-23 23:03:18 +0900

    kwangtaek/240823/12916

---
+++ personal_problem/kwangtaek/level01/240823/Programmers12916.java
@@ -0,0 +1,22 @@
+package level01;
+
+public class Programmers12916 {
+
+    public boolean solution(String s) {
+        boolean answer = true;
+        int p = 0;
+        int y = 0;
+
+        s = s.toLowerCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'p') p++;
+            else if (s.charAt(i) == 'y') y++;
+        }
+
+        if (p != y) answer = false;
+
+        return answer;
+    }
+
+}
